dae689d73f1665bc4f4cf181b9015cf2f2e6a854
Wonkeun No <zakk23v@hotmail.com>
2022-10-26 16:26:27 -0400

    Add sidebar support for multiple file input

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -86,5 +90,65 @@ public class Parser {
         bodyContent = new StringBuilder(bodyContent.substring(0, bodyContent.length() - 1));
 
         return bodyContent.toString();
+    }
+
+    public static String buildSidebar(ArrayList<String> convertingFiles) {
+        StringBuilder sidebar = new StringBuilder();
+        DOMNode<String> rootDom = null;
+        sidebar.append("<div class=\"sidebar\"><ul>");
+
+        for (String file : convertingFiles){
+            String trimmedFilename = removeExtension(file.replace("./", ""));
+            String[] filePathSplit = trimmedFilename.split("/");
+
+            List<DOMNode<String>> domNodes = new ArrayList<>(filePathSplit.length-1);
+            for (int i=0; i<filePathSplit.length; i++){
+                if (rootDom == null && i == 0){
+                    rootDom = new DOMNode<>(filePathSplit[0]);
+                    sidebar.append(filePathSplit[0]);
+                } else {
+                    domNodes.add(new DOMNode<>(filePathSplit[i]));
+
+                    if (i == 1) {
+                        if (!rootDom.doesChildrenHaveData(filePathSplit[i])) {
+                            rootDom.addChild(filePathSplit[i]);
+                        }
+                    } else {
+                        DOMNode<String> tempNode = rootDom;
+                        int j = 1;
+
+                        while (j <= i) {
+                            if (tempNode.getChild(filePathSplit[j]) == null) {
+                                tempNode.addChild(filePathSplit[j]);
+                            }
+                            tempNode = tempNode.getChild(filePathSplit[j]);
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+        sidebar.append(renderList(rootDom));
+        sidebar.append("</ul>\n</div>");
+        return sidebar.toString();
+    }
+
+    private static String renderList(DOMNode<String> rootDom) {
+        StringBuilder renderedList = new StringBuilder();
+
+        for (DOMNode<String> child : rootDom.getChildren()){
+            if (child.isLeaf()){
+                renderedList.append("<li><a href=\"" + child.getUrl() + "\">" + child.getData() + "</a></li>\n");
+            } else {
+                renderedList.append("<li>" + child.getData());
+                renderedList.append("<ul>" + renderList(child) + "</ul>");
+                renderedList.append("</li>");
+            }
+        }
+        return renderedList.toString();
+    }
+
+    private static String removeExtension(String filename) {
+        return filename.substring(0, filename.lastIndexOf("."));
     }
 }
