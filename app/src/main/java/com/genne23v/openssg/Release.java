5665ce37f4914a7d91b9717801b297b36f68594b
Wonkeun No <zakk23v@hotmail.com>
2022-11-25 22:03:06 -0500

    Version update

--- app/src/main/java/com/genne23v/openssg/Release.java
+++ app/src/main/java/com/genne23v/openssg/Release.java
@@ -2,8 +2,8 @@ package com.genne23v.openssg;
 
 public class Release {
     static final String NAME = "OpenSSG";
-    static final String VERSION = "0.4.1";
-    static final String DATE_OF_RELEASE = "2022-11-04";
+    static final String VERSION = "0.9.2";
+    static final String DATE_OF_RELEASE = "2022-11-25";
 
     private Release() {}
 }
